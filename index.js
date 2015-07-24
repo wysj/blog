7d4a9dc4e2bbc0b548a23ffe5ad215af3f877a3a
Your Name <41635861@qq.com>
2015-07-24 17:49:02 +0800

    测试cookie

--- index.js
+++ index.js
@@ -1,32 +1,22 @@
 var express = require('express');
-var cheerio = require('cheerio');
-var superagent = require('superagent');
+var cookieParser = require('cookie-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function (req, res, next) {
-  superagent.get('https://cnodejs.org/')
-    .end(function (err, sres) {
-      if (err) {
-        return next(err);
-      }
-      var $ = cheerio.load(sres.text);
-      var items = [];
-      $('#topic_list .topic_title').each(function (idx, element) {
-        var $element = $(element);
-        items.push({
-          title: $element.attr('title'),
-          href: $element.attr('href')
+app.use(cookieParser());
+app.get('/', function(req, res, next) {
+    if (req.cookies.isVisit) {
+        console.log(req.cookies);
+        res.send("Welcome to mmz nodejs project...again..");
+    } else {
+        res.cookie('isVisit', 1, {
+            maxAge: 60 * 1000
         });
-      });
-
-      res.send(items);
-    });
+        res.send("Welcome to mmz nodejs project...");
+    }
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
