a5a1d4773646c556f5cd448b167a0ac50340314b
Your Name <41635861@qq.com>
2015-07-29 14:32:41 +0800

    构建基本的博客

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
   cookieSecret: 'myblog',
-  // url: 'mongodb://leifeng:zym199101@ds045521.mongolab.com:45521/mmzwy'
-  url : 'mongodb://localhost:27017/myBlog'
+   url: 'mongodb://leifeng:zym199101@ds045521.mongolab.com:45521/mmzwy'
+  //url : 'mongodb://localhost:27017/myBlog'
 };
